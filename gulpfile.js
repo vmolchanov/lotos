617ae6d983e68b0a3db2ea6ac45555bb613010d1
Vladislav Molchanov <Molchanov96@yandex.ru>
2019-09-02 23:55:00 +0300

    Фикс сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const browserSync = require("browser-sync").create();
+const browserSync = require('./gulp-tasks/browser-sync');
 
 const scripts = require('./gulp-tasks/scripts');
 const styles = require('./gulp-tasks/styles');
@@ -14,25 +14,17 @@ gulp.task('styles', styles);
 
 gulp.task('images', images);
 
-gulp.task('serve', () => {
-    browserSync.init({
-        server: './build'
-    });
-
-    gulp.watch('./src/templates/**/*.jade', gulp.series('templates-watch'));
-    gulp.watch('./src/scss/**/*.scss', gulp.series('styles-watch'));
-    gulp.watch('./src/js/**/*.js', gulp.series('scripts-watch'));
+gulp.task('reload', (done) => {
+    browserSync.reload();
+    done();
 });
 
-const watcher = {
-    'templates-watch': ['templates'],
-    'styles-watch': ['styles'],
-    'scripts-watch': ['scripts']
-};
-
-Object.keys(watcher).forEach((name) => {
-    gulp.task(name, gulp.series(...watcher[name]), (done) => {
-        browserSync.reload();
-        done();
+gulp.task('serve', () => {
+    browserSync.init({
+        server: 'build'
     });
+
+    gulp.watch('./src/templates/**/*.jade', gulp.series('templates', 'reload'));
+    gulp.watch('./src/scss/**/*.scss', gulp.series('styles', 'reload'));
+    gulp.watch('./src/js/**/*.js', gulp.series('scripts', 'reload'));
 });
